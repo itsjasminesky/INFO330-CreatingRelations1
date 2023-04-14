e75643bd90f353fdfd96d639b5b160d4bb97647f
itsjasminesky <107975518+itsjasminesky@users.noreply.github.com>
2023-04-13 23:55:15 -0700

    Update courses.sql

--- courses.sql
+++ courses.sql
@@ -7,9 +7,9 @@ description VARCHAR(400);
 );
 
 INSERT INTO courses (code, start, end, description) VALUES
-	('INFO330A', '2023-04-01', '2023-06-01', 'Data and databases'),
-  ('INFO314', '2023-04-01', '2023-06-01', 'Networking and distributed Systems'),
-  ('INFO448A', '2023-09-25', '2023-12-19', 'Introduction to iOS'),
-  ('INFO449A', '2023-09-25', '2023-12-19', 'Introduction to Android'),
-  ('BAW0100', '2023-04-01', '2023-06-01', 'Introduction to Basket-Weaving'),
-  ('BAW100A', '2023-04-01', '2023-06-01', 'Underwater Basket-Weaving');
+('INFO330A', '2023-04-01', '2023-06-01', 'Data and databases'),
+('INFO314', '2023-04-01', '2023-06-01', 'Networking and distributed Systems'),
+('INFO448A', '2023-09-25', '2023-12-19', 'Introduction to iOS'),
+('INFO449A', '2023-09-25', '2023-12-19', 'Introduction to Android'),
+('BAW0100', '2023-04-01', '2023-06-01', 'Introduction to Basket-Weaving'),
+('BAW100A', '2023-04-01', '2023-06-01', 'Underwater Basket-Weaving');
